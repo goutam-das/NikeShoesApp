b124e32ed941b0dd0a6af240be8f2cc48141f4d2
Goutam Das <goutam@stockedge.com>
2021-01-04 11:29:53 +0530

    assets links

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,6 @@
-export default {}
+export default {}  
+import icons from "./icons";
+import images from "./images";
+import theme, { COLORS, SIZES, FONTS } from "./theme";
+
+export { icons, images, theme, COLORS, SIZES, FONTS };
